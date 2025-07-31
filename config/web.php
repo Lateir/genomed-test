14fc1a12bdcad81c6a5e7b30eff396a7e34d5c05
Даниил <lfybnz123@gmail.com>
2025-07-31 17:07:55 +0500

    Финальный коммит

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'LlazienvtZol7fiIV_tlggPAaDUIzYiK',
             'trustedHosts' => [
                 '172.17.0.0/16',    // default Docker bridge
                 '10.0.0.0/8',
